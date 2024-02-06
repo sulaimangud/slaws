91499c63a1290451a1e942b129308e126d7dcca8
sulaimanx <68037537+sulaimanx@users.noreply.github.com>
2024-02-06 09:18:33 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '190b512e-baae-414a-8cad-64d27a158c7d';
 
 const proxyIPs = ['108.137.108.25'];
 
